d125312a86171f6f5c2b177bee13ea22f46b21e1
xinjundong <xinjundong@sohu.com>
2016-07-04 14:30:46 +0800

    modify tapEvent.js

--- commen/tapEvent.js
+++ commen/tapEvent.js
@@ -1,7 +1,38 @@
 // tap事件  移动端模拟click事件
 define(function(){
+   var classType = 3;// 3:tag, 1:class, 2:id
     function addEvent(dom, type, callback) {
 		dom.addEventListener(type, callback);
+	}
+	//返回的事件对象
+	var eventObj= {};
+	function getSelector(tar,type) {
+		var style;
+		switch(type) {
+			case 1: 
+				style = tar.className;
+				break;
+			case 2:
+				style = tar.id;
+				break;
+			case 3:
+				style = tar.tagName.toLowerCase();
+				
+		}
+		return style;
+	}
+	function getTarget(tar, className, type){
+		var style= getSelector(tar, type);
+
+		while(style.indexOf($.trim(className)) == -1) {
+			if(tar.flag) {
+				return null;
+			}
+			
+			tar = tar.parentNode;
+			style= getSelector(tar, type);
+		}
+		return tar;
 	}
 	/**
 	* 移动端模拟click事件
@@ -11,15 +42,29 @@ define(function(){
 	*  className： dom子元素的样式类名
 	*
 	*/
-	function addTapEvent(dom, callback, className) {
-		var x, y;
+	return function(dom, callback, className) {
+		var x, y, startTime, endTime, deltaTime = 0;
+		dom.flag = 'parent';
+		if(className) {
+			if(className.indexOf('.') == 0) {
+				classType = 1;
+				className = className.replace('.', '');
+			} else if(className.indexOf('#') == 0){
+				classType = 2;
+				className = className.replace('#', '');
+			} else {
+				classType = 3;
+			}
+		}
 		addEvent(dom, 'touchstart', function (e) {
 			e.preventDefault();
-			var t = null, style;
+			startTime = Date.now();
+			var t = null, tar;
 			if(className) {
 				t = e.target;
-				style = t.className;
-				if(style.indexOf(className) > -1) {
+				tar = getTarget(t, className, classType);
+				
+				if(tar) {
 					x = e.touches[0].clientX;
 					y = e.touches[0].clientY;
 				}
@@ -33,18 +78,23 @@ define(function(){
 			e.preventDefault();
 			var x1 = e.changedTouches[0].clientX;
 			var y1 = e.changedTouches[0].clientY;
-			var t, style;
+			var t, tar;
+			endTime = Date.now();
+			deltaTime = endTime - startTime;
 			if(className) {
 				t = e.target;
-				style = t.className;
-				if(style.indexOf(className) > -1) {
-					if(Math.abs(x1 - x) < 5 && Math.abs(y1 - y) < 5) {
-						callback(e);
+				tar = getTarget(t, className, classType);
+				if(tar) {
+					
+					if(Math.abs(x1 - x) < 5 && Math.abs(y1 - y) < 5 && deltaTime > 80) {
+						eventObj.target = tar;
+						callback(eventObj);
 					}
 				}
 			} else {
-				if(Math.abs(x1 - x) < 5 && Math.abs(y1 - y) < 5) {
-					callback(e);
+				if(Math.abs(x1 - x) < 5 && Math.abs(y1 - y) < 5 && deltaTime > 80) {
+					eventObj.target = e.target;
+					callback(eventObj);
 				}
 			}
 		});
